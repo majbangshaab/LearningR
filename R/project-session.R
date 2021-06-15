fae8cac4bb77fb1eb8be831e4db68f1a80d4815d
Maj Bangshaab <maj@bangshaab.dk>
2021-06-15 15:15:40 +0200

    Start of project

---
+++ R/project-session.R
@@ -0,0 +1,40 @@
+10
+
+# Basics of R -------------------------------------------------------------
+
+
+weight_kilos <- 100
+weight_kilos
+
+c("a", "b", "c")
+c(TRUE, FALSE)
+c(1, 2, 4)
+factor(c("high", "medium", "low"))
+
+head(CO2, 1)
+colnames(CO2)
+str(CO2)
+summary(CO2)
+
+colnames(CO2)
+
+# Object names
+day_one
+TRUE <- FALSE
+c(9
+
+  # Spacing
+  x[, 1]
+  x[, 1]
+  mean(x, na.rm = TRUE)
+  mean(x, na.rm = TRUE)
+  height <- feet * 12 + inches
+  df$z
+  x <- 1:10
+
+  # Indenting and brackets
+  if (y < 0 && debug) {
+      message("Y is negative")
+  }
+
+  styler::style_file(project-session)
