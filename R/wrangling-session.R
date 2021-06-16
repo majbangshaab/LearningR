d9a162dfc1f03378fa43430dc291b3bafcc7274f
Maj Bangshaab <maj@bangshaab.dk>
2021-06-16 11:04:47 +0200

    Renaming and pipe function

--- R/wrangling-session.R
+++ R/wrangling-session.R
@@ -3,3 +3,54 @@ source(here::here("R/package-loading.R"))
 
 # Briefly glimpse content of dataset
 glimpse(NHANES)
+
+select(NHANES, Age)
+
+# Select more columns
+select(NHANES, Age, Weight, BMI)
+
+# Exclude a column
+select(NHANES, -HeadCirc)
+
+#all columns starting with "BP"
+select(NHANES, starts_with("BP"))
+
+#All columns ending with "Day"
+select(NHANES, ends_with("Day"))
+
+#All columns that contain "age"
+select(NHANES, contains("Age"))
+
+?select_helpers
+
+#Save the selected columns as a new data frame
+nhanes_small <- select(NHANES, Age, Gender, Height, Weight, BMI, Diabetes, DiabetesAge,
+                       PhysActiveDays, PhysActive, TotChol,
+                       BPSysAve, BPDiaAve, SmokeNow, Poverty)
+
+#View the new data frame
+nhanes_small
+
+## Renaming
+# Rename all columns to snake case
+nhanes_small <- rename_with(nhanes_small, snakecase::to_snake_case)
+
+# Renaming specific columns
+rename(nhanes_small, sex = gender)
+
+nhanes_small
+
+nhanes_small <- rename(nhanes_small, sex = gender)
+
+nhanes_small
+
+# The pipe operator
+#without the pipe operator
+colnames(nhanes_small)
+#pipe operator: (ctrl. + shift + M)
+nhanes_small %>% colnames()
+
+# Using the pipe operator with more functions
+nhanes_small %>%
+    select(phys_active) %>%
+    rename(physically_active = phys_active)
